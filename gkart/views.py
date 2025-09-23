c552b16b71b3c374ef7c87b38d9b36e04a94849b
Suraj Gupta <surajgupta689800@gmail.com>
2025-09-23 14:19:42 +0530

    updated the home views

--- gkart/views.py
+++ gkart/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from store.models import Product, ReviewRating
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='login')
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_at')
 
