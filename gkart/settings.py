22042fbb212afabc684f06cc0001eda0e9dbc931
Suraj Gupta <surajgupta689800@gmail.com>
2025-09-28 15:12:34 +0530

    static update

--- gkart/settings.py
+++ gkart/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
-    'storages'
 ]
 
 MIDDLEWARE = [
@@ -96,30 +95,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-
-# STATIC_URL = "/static/"
-# STATICFILES_DIRS = [BASE_DIR / "gkart/static"]
-# STATIC_ROOT = BASE_DIR / "static"
-
-# AWS S3 Static Files Configuration
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-AWS_LOCATION = 'static'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    "gkart/static"
+]
 
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
