7df5243ea87cc4f77dfcda472fc611712209d901
Suraj Gupta <surajgupta689800@gmail.com>
2025-10-11 22:42:13 +0530

    dump sqllite dato into rds db

--- gkart/urls.py
+++ gkart/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('suraj-admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
